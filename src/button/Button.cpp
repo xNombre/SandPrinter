8fb1d4bf5ef305812fb35fca39924bf212799439
Andrzej Perczak <linux@andrzejperczak.com>
2023-01-11 00:08:42 +0100

    Button: disable gpio interrupt when debouncing
    This makes callback to fire on button down rather than up - cpu doesnt
    get fired by multiple interrupts and can proceed with code execution.

--- src/button/Button.cpp
+++ src/button/Button.cpp
@@ -6,13 +6,19 @@
 
 std::map<uint8_t, Button *> Button::gpio_to_instance_map;
 
+namespace Constant
+{
+    const uint32_t button_irq_event_mask = GPIO_IRQ_EDGE_FALL;
+}
+
 Button::Button(uint8_t gpio)
     : gpio(gpio),
     alarm_id(0)
 {
     gpio_init(gpio);
+    gpio_set_dir(gpio, false);
     gpio_pull_up(gpio);
-    gpio_set_irq_enabled_with_callback(gpio, GPIO_IRQ_LEVEL_LOW | GPIO_IRQ_EDGE_FALL, true, handle_irq);
+    gpio_set_irq_enabled_with_callback(gpio, Constant::button_irq_event_mask, true, handle_irq);
 
     gpio_to_instance_map[gpio] = this;
 }
@@ -40,8 +46,10 @@ void Button::handle_irq(uint gpio, uint32_t event)
     if (instance->alarm_id != 0) {
         cancel_alarm(instance->alarm_id);
     }
-    else if (instance->event_callback) {
-        instance->event_callback();
+    else {
+        gpio_set_irq_enabled(gpio, Constant::button_irq_event_mask, false);
+        if (instance->event_callback)
+            instance->event_callback();
     }
 
     instance->alarm_id = add_alarm_in_ms(Constants::BUTTON_DEBOUNCE_MS, enable_button, instance, false);
@@ -51,7 +59,11 @@ int64_t Button::enable_button(alarm_id_t alarm_id, void *user_data)
 {
     auto instance = (Button *)user_data;
 
+    if (!gpio_get(instance->gpio))
+        return Constants::BUTTON_DEBOUNCE_MS;
+
     instance->alarm_id = 0;
+    gpio_set_irq_enabled(instance->gpio, Constant::button_irq_event_mask, true);
 
     return 0;
 }
--- src/button/Button.hpp
+++ src/button/Button.hpp
@@ -24,19 +24,7 @@ public:
     void set_callback(callback_t callback);
     void remove_callback();
 
-    // Has to be called periodically
-    // Return true if any button event has been registered
-    //bool update_buttons();
-
-    // Carefull, blocks until event is registered
-    //void wait_for_button_event(bool use_tight_loop = false);
-
 private:
-    //std::shared_ptr<Button> instance;
-
-    //static std::queue<uint8_t> events;
-    //std::unordered_map<uint8_t, callback_t> button_callbacks;
-
     uint8_t gpio;
     callback_t event_callback;
     alarm_id_t alarm_id;
