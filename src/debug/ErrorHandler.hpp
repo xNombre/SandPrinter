775b693eb65b1d3ba7694313151e5aa93e9af847
Andrzej Perczak <linux@andrzejperczak.com>
2023-01-12 00:20:25 +0100

    Introduce ErrorHandler
    Written to preserve gpio safety after panic.

--- src/debug/ErrorHandler.hpp
+++ src/debug/ErrorHandler.hpp
@@ -4,8 +4,26 @@
 
 #include "pico/stdlib.h"
 
-void fatal_error(const std::string &msg)
+#include "../display/Display.hpp"
+#include "../config/Constants.hpp"
+
+const inline uint32_t irqs_to_mask = TIMER_IRQ_0 | TIMER_IRQ_1 | TIMER_IRQ_2 | TIMER_IRQ_3;
+const inline uint32_t all_irq_levels = GPIO_IRQ_LEVEL_LOW | GPIO_IRQ_LEVEL_HIGH | GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE;
+
+//void __time_critical_func(fatal_error)(const std::string &msg)
+static void fatal_error(const std::string &msg)
 {
-    // get display service and display error message
-    panic(msg.c_str());
+    irq_set_mask_enabled(irqs_to_mask, false);
+    gpio_set_irq_enabled(Constants::BUTTON_DOWN_GPIO, all_irq_levels, false);
+    gpio_set_irq_enabled(Constants::BUTTON_OK_GPIO, all_irq_levels, false);
+    gpio_set_irq_enabled(Constants::BUTTON_UP_GPIO, all_irq_levels, false);
+    
+    auto display_instance = Display::get_instance();
+    display_instance->clear();
+    display_instance->print_line("System panic!");
+    display_instance->print_line(msg);
+
+    gpio_clr_mask(0x00);
+
+    panic("");
 }
