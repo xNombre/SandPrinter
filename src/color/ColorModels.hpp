f12c8e619035d39aa68d0a6289e17cf4e0fc67fa
Andrzej Perczak <linux@andrzejperczak.com>
2023-03-06 21:01:35 +0100

    ColorModels: Fix RGB to CMYK conversion

--- src/color/ColorModels.hpp
+++ src/color/ColorModels.hpp
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <algorithm>
 
+#include <pico/double.h>
+
 struct rgb {
     uint8_t r;
     uint8_t g;
@@ -22,12 +24,25 @@ struct cmyk {
 
     cmyk(const rgb &val)
     {
-        rgb inverted_rgb(val.r / 255, val.g / 255, val.b / 255);
+        double r_fill, g_fill, b_fill;
+        r_fill = (double)val.r / 255.0;
+        g_fill = (double)val.g / 255.0;
+        b_fill = (double)val.b / 255.0;
 
-        k = 1 - std::max(inverted_rgb.r, std::max(inverted_rgb.g, inverted_rgb.b));
-        c = (1 - inverted_rgb.r - k) / (1 - k);
-        m = (1 - inverted_rgb.g - k) / (1 - k);
-        y = (1 - inverted_rgb.b - k) / (1 - k);
+        double inverted_black = std::max(r_fill, std::max(g_fill, b_fill));
+        double black = 1.0 - inverted_black;
+        if (inverted_black == 0.0) {
+            c = 0;
+            m = 0;
+            y = 0;
+        }
+        else {
+            c = std::round(100.0 * (inverted_black - r_fill) / inverted_black);
+            m = std::round(100.0 * (inverted_black - g_fill) / inverted_black);
+            y = std::round(100.0 * (inverted_black - b_fill) / inverted_black);
+        }
+        
+        k = std::round(100.0 * black);
     }
     
     cmyk(uint8_t c = 0, uint8_t m = 0, uint8_t y = 0, uint8_t k = 0)
